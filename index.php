d02a16cc621453139889c6b2c28266f89630e493
Th1nh2411 <rinktvn2525@gmail.com>
2024-07-11 17:00:12 +0700

    update readme

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: http://localhost:8888');
+header('Access-Control-Allow-Origin: http://localhost:8000');
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
